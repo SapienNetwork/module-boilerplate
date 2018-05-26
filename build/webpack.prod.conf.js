a0fd1df76a3c9449d1d2ba35c1c93a91ca5f27c5
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2018-05-25 23:25:05 -0500

    Adding build script

---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const utils = require('./utils');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+
+module.exports = merge(baseWebpackConfig, {
+  entry: './src/index.js',
+  output: {
+    path: utils.resolve('dist'),
+    filename: 'index.js',
+  },
+})
