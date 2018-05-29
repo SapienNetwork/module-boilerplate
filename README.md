7147bffcda24078890f63c0069b1ad6a74e03a4e
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2018-05-28 22:17:05 -0500

    Better install-peers script

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 ```bash
 yarn dev
 ```
+## Lint
+```bash
+# lint all the things
+yarn lint
+```
+
+## Install-peers
+```bash
+# Install all peers dependencies as local dependencies to be used on development
+yarn install-peers
+```
--- build/install-peers.js
+++ build/install-peers.js
@@ -1,22 +1,25 @@
+const ora = require('ora');
 const path = require('path');
 const pkt = require('../package.json');
 const cp = require('child_process');
+var executioner = require('executioner');
 
 const { peerDependencies } = pkt;
+
+const rootFolder = path.join(__dirname, '..');
+
+const spinner = ora('Installing Peers as Dev...');
+spinner.start();
 
 const peersNames = Object.entries(peerDependencies)
   .map(d => d.join('@'))
   .join(' ');
 
-const rootFolder = path.join(__dirname, '..');
-console.log(peersNames);
-cp.spawn('yarn', [
-  'add',
-  '-P',
-  '--no-lockfile',
-  peersNames
-], {
-  cwd: rootFolder,
-  stdio: 'inherit',
-  env: process.env,
-});
+  executioner(`yarn add --peer --no-lockfile ${peersNames}`, {}, (error, result) => {
+    spinner.stop();
+    if (error) {
+      console.error('Unable to install peerDependencies', error);
+      process.exit(1);
+      return;
+    }
+  });
